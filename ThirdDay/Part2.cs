3f970bc6d744883fb0584c6e1b3cf77e5e1d13c6
Igor <igormiekina@gmail.com>
2023-12-10 05:06:24 +0100

    part2 third day

---
+++ ThirdDay/Part2.cs
@@ -0,0 +1,144 @@
+﻿namespace ThirdDay;
+
+public class Part2
+{
+    public void Execute()
+    {
+        const string rootPath = @"C:\Users\Igor\RiderProjects\Aoc2023\ThirdDay";
+        var file = new StreamReader($"{rootPath}/input.txt");
+        Console.WriteLine(Solve(file));
+        
+    }
+    int Solve(StreamReader file)
+    {
+        var context = new Context(null, null, null);
+        string? lastLine = null;
+        int sum = 0;
+        
+        while (file.ReadLine() is {} line)
+        {
+            var currentNums = GetNumsFromLine(line);
+            context = new Context(context.CurrentLineNumbers, context.NextLineNumbers, currentNums);
+            sum += HandleContext(context, lastLine);
+            lastLine = line;
+        }
+    
+        context = new Context(context.CurrentLineNumbers, context.NextLineNumbers, null);
+        sum += HandleContext(context, null);
+    
+        return sum;
+    }
+    int HandleContext(Context context, string? line)
+    {
+        if (context.CurrentLineNumbers is null)
+        {
+            return 0;
+        }
+    
+        ReadOnlySpan<char> lineSpan;
+        if (line is not null)
+        {
+            lineSpan = line.AsSpan();
+        }
+        else
+        {
+            lineSpan = new ReadOnlySpan<char>();
+        }
+        var startAt = 0;
+        var sum = 0;
+    
+        while (ReadStar(lineSpan, startAt) is {} read)
+        {
+            startAt = read.endIndex;
+            var numbers = GetNumbersFromArea(context, read.starPosition);
+            if (numbers.Length == 2)
+            {
+                sum += numbers.Select(x => x.Result).Aggregate((x, y) => x * y);
+            }
+        }
+        return sum;
+    }
+    NumberDef? ReadNumber(ReadOnlySpan<char> text, int startAt)
+    {
+        var index = startAt;
+    
+        while (text.Length > index && !char.IsDigit(text[index]))
+        {
+            index++;
+        }
+    
+        if (text.Length <= index)
+        {
+            return null;
+        }
+    
+        var endIndex = index;
+    
+        while (text.Length > endIndex && char.IsDigit(text[endIndex]))
+        {
+            endIndex++;
+        }
+    
+        return new NumberDef(int.Parse(text[index..endIndex]), Math.Max(0, index - 1), endIndex);
+    }
+    NumberDef[] GetNumsFromLine(string line)
+    {
+        var lineSpan = line.AsSpan();
+        var startAt = 0;
+        var nums = new List<NumberDef>();
+    
+        while (ReadNumber(lineSpan, startAt) is {} read)
+        {
+            startAt = read.EndIndex;
+            var end = Math.Min(line.Length - 1, read.EndIndex);
+            var readNumber = new NumberDef(read.Result, read.StartIndex, end);
+            nums.Add(readNumber);
+        }
+    
+        return nums.ToArray();
+    }
+    StarDef? ReadStar(ReadOnlySpan<char> text, int startAt)
+    {
+        var index = startAt;
+    
+        for (int i = index; i < text.Length; i++)
+        {
+            if (text[i] == '*')
+            {
+                return new StarDef(i, i + 1);
+            }
+        }
+    
+        return null;
+    }
+    NumberDef[] GetNumbersFromArea(Context context, int starPosition)
+    {
+        return GetNumbersFromLine(context.PreviousLineNumbers)
+            .Concat(GetNumbersFromLine(context.NextLineNumbers))
+            .Concat(GetNumbersFromLine(context.CurrentLineNumbers))
+            .Where(x =>
+            {
+                var start =x.StartIndex + 1;
+                var end = x.EndIndex - 1;
+                return (start >= starPosition - 1 && start <= starPosition + 1) ||
+                       (end >= starPosition - 1 && end <= starPosition + 1);
+            })
+            .ToArray();
+    }
+    
+    NumberDef[] GetNumbersFromLine(NumberDef[]? numberDef)
+    {
+        if (numberDef is {} number)
+        {
+            return number;
+        }
+    
+        return new NumberDef[] { };
+    }
+    
+    record struct Context(NumberDef[]? PreviousLineNumbers, NumberDef[]? CurrentLineNumbers, NumberDef[]? NextLineNumbers);
+    
+    record struct NumberDef(int Result, int StartIndex, int EndIndex);
+    
+    record struct StarDef(int starPosition, int endIndex);
+}
